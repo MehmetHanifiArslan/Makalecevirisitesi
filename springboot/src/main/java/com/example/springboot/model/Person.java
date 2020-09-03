7483d33899f309038286a952f9391207a24a2df0
MehmetHanifiArslan <65266211+MehmetHanifiArslan@users.noreply.github.com>
2020-09-03 13:12:11 +0300

    springboot

---
+++ springboot/src/main/java/com/example/springboot/model/Person.java
@@ -0,0 +1,78 @@
+package com.example.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private long phone;
+	
+	public Person() {
+		
+	}
+	
+	public Person(String firstName, String lastName, String emailId,long phone) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.phone = phone;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "first_name", nullable = false)
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	@Column(name = "last_name", nullable = false)
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Column(name = "email_address", nullable = false)
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	@Column(name = "phone", nullable = false)
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ ",phone=" + phone +"]";
+	}
+	
+}
